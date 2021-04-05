783cff4314912e4dbd430414dca4c1cd0be98caf
lotemn <lotemn@T-P-LOTEMN-WW>
2021-04-05 14:57:31 +0300

    Added distance calc & radius calc

--- bpa.py
+++ bpa.py
@@ -1,10 +1,12 @@
 from grid import Grid, Point
+import math
 
 
 class BPA:
     def __init__(self, path, radius):
-        points = self._read_points(path)
-        self.grid = Grid(points=points, radius=radius)
+        pass
+        #points = self._read_points(path)
+        #self.grid = Grid(points=points, radius=radius)
 
     @staticmethod
     def _read_points(path):
@@ -23,10 +25,44 @@ class BPA:
 
         return points
 
+    @staticmethod
+    def calc_incircle_radius(p1: Point, p2: Point, p3: Point):
+        """
+        Calculate the radius of the incircle in a triangle.
+        Based on this formula:
+         https://en.wikipedia.org/wiki/Incircle_and_excircles_of_a_triangle#Radius
+
+        :param p1: First point of triangle.
+        :param p2: Second point of triangle.
+        :param p3: Third point of triangle.
+        :return: The radius of the incircle.
+        """
+
+        edge_1_length = BPA.calc_distance(p1, p2)
+        edge_2_length = BPA.calc_distance(p2, p3)
+        edge_3_length = BPA.calc_distance(p1, p3)
+
+        s = (edge_1_length + edge_2_length + edge_3_length) / 2
+        r = math.sqrt(((s - edge_1_length)*(s - edge_2_length)*(s - edge_3_length)) / s)
+        return r
+
+    @staticmethod
+    def calc_distance(p1: Point, p2: Point):
+        """
+        Calculate the distance between 2 3D points.
+
+        :param p1: First point.
+        :param p2: Second point.
+        :return: Distance between the points.
+        """
+        return math.sqrt(math.pow((p2.x - p1.x), 2) + math.pow((p2.y - p1.y), 2) + math.pow((p2.z - p1.z), 2))
+
     def _find_seed_triangle(self):
         pass
 
     def create_mesh(self):
         pass
+
+
 
 bpa = BPA('bunny.pcd', 0.5)
---
+++ tests/test_bpa.py
@@ -0,0 +1,34 @@
+import unittest
+from bpa import BPA
+from grid import Point
+
+
+class TestBPA(unittest.TestCase):
+    def test_distance(self):
+        p1 = Point(1, 1, 0)
+        p2 = Point(2, 1, 2)
+        d = BPA.calc_distance(p1, p2)
+        self.assertEqual(round(d, 2), 2.24)
+
+        p3 = Point(0, 0, 0)
+        p4 = Point(-1, 1, 2)
+        d = BPA.calc_distance(p3, p4)
+        self.assertEqual(round(d, 2), 2.45)
+
+    def test_incircle_radius(self):
+        p1 = Point(1, 1, 1)
+        p2 = Point(0, 2, 0)
+        p3 = Point(3, 3, 3)
+
+        r = BPA.calc_incircle_radius(p1, p2, p3)
+        self.assertEqual(round(r, 3), 0.592)
+
+        p1 = Point(1, 1, 1)
+        p2 = Point(0, -1, 0)
+        p3 = Point(2, -1, 3)
+
+        r = BPA.calc_incircle_radius(p1, p2, p3)
+        self.assertEqual(round(r, 3), 0.804)
+
+
+
